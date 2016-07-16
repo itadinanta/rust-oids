5d98e02570cd10aa5d3213dd46872aae96cb05a6
Nico Orru <nigu.orru@gmail.com>
2016-07-16 18:32:04 +0100

    Splitting shader

--- src/render/forward.rs
+++ src/render/forward.rs
@@ -47,7 +47,7 @@ void main() {
 ";
 const MAX_NUM_TOTAL_LIGHTS: usize = 16;
 
-pub static LIGHTING_FRAGMENT_SRC: &'static [u8] = b"
+pub static LIGHTING_BALL_FRAGMENT_SRC: &'static [u8] = b"
 
 #version 150 core
 #define MAX_NUM_TOTAL_LIGHTS 16
@@ -139,6 +139,99 @@ void main() {
 
 ";
 
+pub static LIGHTING_POLY_FRAGMENT_SRC: &'static [u8] = b"
+
+#version 150 core
+#define MAX_NUM_TOTAL_LIGHTS 16
+
+const float PI = 3.1415926535897932384626433832795;
+const float PI_2 = 1.57079632679489661923;
+
+layout (std140) uniform cb_FragmentArgs {
+    int u_LightCount;
+};
+
+struct Light {
+    vec4 propagation;
+    vec4 center;
+    vec4 color;
+};
+
+layout (std140) uniform u_Lights {
+    Light light[MAX_NUM_TOTAL_LIGHTS];
+};
+
+layout (std140) uniform cb_MaterialArgs {
+    uniform vec4 u_Emissive;
+};
+
+in VertexData {
+    vec4 Position;
+    vec3 Normal;
+    mat3 TBN;
+    vec2 TexCoord;
+} v_In;
+
+out vec4 o_Color;
+
+void main() {
+    vec4 kd = vec4(0.2, 0.2, 0.2, 1.0);
+    vec4 ks = vec4(1.0, 1.0, 1.0, 1.0);
+    vec4 kp = vec4(64.0, 32.0, 64.0, 64.0);
+    vec4 ka = vec4(0.0, 0.0, 0.01, 0.0);
+
+    vec4 color = (ka + u_Emissive);
+
+    float dx = v_In.TexCoord.x - 0.5;
+    float dy = v_In.TexCoord.y - 0.5;
+	float r = dx * dx + dy * dy;
+	vec3 normal_map = vec3(0., 0., 1.);
+    if (r > 0.25) {
+        discard;
+    } 
+    else {
+	    dx *= 2;
+	    dy *= 2;
+
+		float bump = sqrt(1. - dx * dx - dy * dy);
+		normal_map = vec3(dx, dy, bump);
+	};
+	
+	vec3 normal = v_In.TBN * normal_map;
+
+	for (int i = 0; i < u_LightCount; i++) {
+		vec4 delta = light[i].center - v_In.Position;
+		float dist = length(delta);
+		float inv_dist = 1. / dist;
+		vec4 light_to_point_normal = delta * inv_dist;
+		float intensity = dot(light[i].propagation.xyz, vec3(1., inv_dist, inv_dist * inv_dist));
+
+		float lambert = max(0, dot(light_to_point_normal, vec4(normal, 0.0)));
+
+		vec4 specular;
+		if (lambert >= 0.0) {
+//			Blinn-Phong:
+			vec3 lightDir = light_to_point_normal.xyz;
+            vec3 viewDir = vec3(0.0, 0.0, 1.0); // ortho, normalize(-v_In.Position.xyz); perspective
+            vec3 halfDir = normalize(lightDir + viewDir); // can be done in vertex shader
+			float specAngle = max(dot(halfDir, normal), 0.0);
+//			Phong:
+//	        vec3 reflectDir = reflect(-lightDir, v_In.Normal.xyz);
+//	        float specAngle = max(dot(reflectDir, viewDir), 0.0);
+			specular = pow(vec4(specAngle), kp);
+		}
+		else {
+			specular = vec4(0.0);
+		}
+		color += light[i].color * intensity * (kd * lambert + ks * specular);
+	}
+	// gl_FragDepth = bump;
+	o_Color = color;
+}
+
+";
+
+
 gfx_vertex_struct!(VertexPosNormal {
 	pos: [f32; 3] = "a_Pos",
 	normal: [f32; 3] = "a_Normal",
@@ -198,7 +291,8 @@ pub struct ForwardLighting<R: gfx::Resources, C: gfx::CommandBuffer<R>> {
 	fragment: gfx::handle::Buffer<R, FragmentArgs>,
 	material: gfx::handle::Buffer<R, MaterialArgs>,
 	lights: gfx::handle::Buffer<R, PointLight>,
-	pso: gfx::pso::PipelineState<R, shaded::Meta>,
+	ball_pso: gfx::pso::PipelineState<R, shaded::Meta>,
+	poly_pso: gfx::pso::PipelineState<R, shaded::Meta>,
 	_buffer: PhantomData<C>,
 }
 
@@ -210,7 +304,15 @@ impl<R: gfx::Resources, C: gfx::CommandBuffer<R>> ForwardLighting<R, C> {
 		let model = factory.create_constant_buffer(1);
 		let fragment = factory.create_constant_buffer(1);
 		let material = factory.create_constant_buffer(1);
-		let pso = factory.create_pipeline_simple(LIGHTING_VERTEX_SRC, LIGHTING_FRAGMENT_SRC, shaded::new()).unwrap();
+		let ball_pso = factory.create_pipeline_simple(LIGHTING_VERTEX_SRC,
+		                                              LIGHTING_BALL_FRAGMENT_SRC,
+		                                              shaded::new())
+		                      .unwrap();
+		let poly_pso = factory.create_pipeline_simple(LIGHTING_VERTEX_SRC,
+		                                              LIGHTING_POLY_FRAGMENT_SRC,
+		                                              shaded::new())
+		                      .unwrap();
+
 
 		ForwardLighting {
 			camera: camera,
@@ -218,7 +320,8 @@ impl<R: gfx::Resources, C: gfx::CommandBuffer<R>> ForwardLighting<R, C> {
 			fragment: fragment,
 			material: material,
 			lights: lights,
-			pso: pso,
+			ball_pso: ball_pso,
+			poly_pso: poly_pso,
 			_buffer: PhantomData,
 		}
 	}
@@ -259,13 +362,32 @@ impl<R: gfx::Resources, C: gfx::CommandBuffer<R>> ForwardLighting<R, C> {
 	                      color: [f32; 4],
 	                      color_buffer: &gfx::handle::RenderTargetView<R, HDRColorFormat>,
 	                      depth_buffer: &gfx::handle::DepthStencilView<R, DepthFormat>) {
+		self.draw_triangles_pso(encoder,
+		                        &self.ball_pso,
+		                        vertices,
+		                        indices,
+		                        transform,
+		                        color,
+		                        color_buffer,
+		                        depth_buffer);
+	}
+
+	fn draw_triangles_pso(&self,
+	                      encoder: &mut gfx::Encoder<R, C>,
+	                      pso: &gfx::PipelineState<R, shaded::Meta>,
+	                      vertices: &gfx::handle::Buffer<R, VertexPosNormal>,
+	                      indices: &gfx::Slice<R>,
+	                      transform: &M44,
+	                      color: [f32; 4],
+	                      color_buffer: &gfx::handle::RenderTargetView<R, HDRColorFormat>,
+	                      depth_buffer: &gfx::handle::DepthStencilView<R, DepthFormat>) {
 
 		encoder.update_constant_buffer(&self.model, &ModelArgs { model: transform.clone().into() });
 
 		encoder.update_constant_buffer(&self.material, &MaterialArgs { emissive: color });
 
 		encoder.draw(&indices,
-		             &self.pso,
+		             &pso,
 		             &shaded::Data {
 			             vbuf: vertices.clone(),
 			             fragment_args: self.fragment.clone(),
