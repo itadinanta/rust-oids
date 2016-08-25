d1753213d6933f5d65614d6a52a1850cd3c11f08
Nicola Orru <nigu.orru@gmail.com>
2016-08-26 00:16:01 +0100

    Christmas-tree-style light animation

--- src/backend/world.rs
+++ src/backend/world.rs
@@ -26,6 +26,7 @@ pub struct State {
 	age_frames: usize,
 	charge: f32,
 	target_charge: f32,
+	recharge: f32,
 	smooth: math::Exponential<f32, f32>,
 	pub intent: Intent,
 	pub collision_detected: bool,
@@ -38,6 +39,7 @@ impl Default for State {
 			age_frames: 0,
 			charge: 1.,
 			target_charge: 0.,
+			recharge: 1.,
 			smooth: math::Exponential::new(1., 1., 2.),
 			intent: Intent::Idle,
 			collision_detected: false,
@@ -63,12 +65,17 @@ impl State {
 		self.age_seconds += dt;
 		self.age_frames += 1;
 		self.charge = self.smooth.dt(dt).smooth(self.target_charge);
+		if (self.charge - self.target_charge).abs() < 0.001 {
+			let reset = self.recharge;
+			self.set_charge(reset);
+		}
 	}
 
-	pub fn with_charge(initial: f32, target: f32) -> Self {
+	pub fn with_charge(initial: f32, target: f32, recharge: f32) -> Self {
 		State {
 			charge: initial,
 			target_charge: target,
+			recharge: recharge,
 			smooth: math::Exponential::new(initial, 1., 2.),
 			..Self::default()
 		}
@@ -480,7 +487,7 @@ impl Flock {
 		let mut builder = AgentBuilder::new(self.next_id(),
 		                                    Material { density: 1.0, ..Default::default() },
 		                                    Livery { albedo: albedo.to_rgba(), ..Default::default() },
-		                                    State::with_charge(charge, 0.));
+		                                    State::with_charge(charge, 0., charge * 0.75));
 		self.insert(builder.start(initial_pos, 0., &ball).build())
 	}
 
@@ -489,7 +496,7 @@ impl Flock {
 		let mut builder = AgentBuilder::new(self.next_id(),
 		                                    Material { density: 0.2, ..Default::default() },
 		                                    Livery { albedo: albedo.to_rgba(), ..Default::default() },
-		                                    State::with_charge(0., charge));
+		                                    State::with_charge(0., charge, charge));
 		let arm_shape = self.rnd.random_star();
 		let leg_shape = self.rnd.random_star();
 		let torso_shape = self.rnd.random_npoly(5, true);
@@ -498,13 +505,13 @@ impl Flock {
 		let initial_angle = consts::PI / 2. + f32::atan2(initial_pos.y, initial_pos.x);
 
 		let torso = builder.start(initial_pos, initial_angle, &torso_shape)
-		                   .index();
+			.index();
 		builder.addr(torso, 2, &arm_shape, ARM | JOINT | ACTUATOR | RUDDER)
-		       .addl(torso, -2, &arm_shape, ARM | JOINT | ACTUATOR | RUDDER);
+			.addl(torso, -2, &arm_shape, ARM | JOINT | ACTUATOR | RUDDER);
 
 		let head = builder.add(torso, 0, &head_shape, HEAD | SENSOR).index();
 		builder.addr(head, 1, &head_shape, HEAD | ACTUATOR | RUDDER)
-		       .addl(head, 2, &head_shape, HEAD | ACTUATOR | RUDDER);
+			.addl(head, 2, &head_shape, HEAD | ACTUATOR | RUDDER);
 
 		let mut belly = torso;
 		let mut belly_mid = torso_shape.mid();
@@ -515,16 +522,16 @@ impl Flock {
 			belly_mid = belly_shape.mid();
 			if self.rnd.irand(0, 4) == 0 {
 				builder.addr(belly, 2, &arm_shape, ARM | ACTUATOR | RUDDER)
-				       .addl(belly, -2, &arm_shape, ARM | ACTUATOR | RUDDER);
+					.addl(belly, -2, &arm_shape, ARM | ACTUATOR | RUDDER);
 			}
 		}
 
 		builder.addr(belly, belly_mid - 1, &leg_shape, LEG | ACTUATOR | THRUSTER)
-		       .addl(belly,
-		             -(belly_mid - 1),
-		             &leg_shape,
-		             LEG | ACTUATOR | THRUSTER)
-		       .add(belly, belly_mid, &tail_shape, TAIL | ACTUATOR | BRAKE);
+			.addl(belly,
+			      -(belly_mid - 1),
+			      &leg_shape,
+			      LEG | ACTUATOR | THRUSTER)
+			.add(belly, belly_mid, &tail_shape, TAIL | ACTUATOR | BRAKE);
 
 		self.insert(builder.build())
 	}
