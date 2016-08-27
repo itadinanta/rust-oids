5504978d4277232085edb0aef0fe57a45fb5204b
Nicola Orru <nigu.orru@gmail.com>
2016-08-27 18:59:59 +0100

    Factored out random generator

---
+++ src/backend/world/gen.rs
@@ -0,0 +1,100 @@
+use num;
+use std::f32::consts;
+use rand;
+use rand::Rng;
+use backend::obj::*;
+
+pub trait Gen {
+	fn new() -> Self;
+
+	fn next_float<T>(&mut self, min: T, max: T) -> T where T: rand::Rand + num::Float;
+
+	fn next_integer<T>(&mut self, min: T, max: T) -> T where T: rand::Rand + num::Integer + Copy;
+}
+
+#[allow(dead_code)]
+pub trait Generator: Gen {
+	fn ball(&mut self) -> Shape {
+		let radius: f32 = self.next_float(1.0, 2.0);
+		Shape::new_ball(radius)
+	}
+
+	fn quad(&mut self) -> Shape {
+		let radius: f32 = self.next_float(1.0, 2.0);
+		let ratio: f32 = self.next_float(1.0, 2.0);
+		Shape::new_box(radius, ratio)
+	}
+
+	fn vbar(&mut self) -> Shape {
+		let radius: f32 = self.next_float(1.0, 2.0);
+		let ratio: f32 = self.next_float(0.1, 0.2);
+		Shape::new_box(radius, ratio)
+	}
+
+	fn triangle(&mut self) -> Shape {
+		let radius = self.next_float(0.5, 1.0);
+		let alpha1 = self.next_float(consts::PI * 0.5, consts::PI * 0.9);
+		let alpha2 = consts::PI * 1.5 - self.next_float(0., consts::PI);
+		Shape::new_triangle(radius, alpha1, alpha2)
+	}
+
+	fn iso_triangle(&mut self) -> Shape {
+		let radius = self.next_float(0.5, 1.0);
+		let alpha1 = self.next_float(consts::PI * 0.5, consts::PI * 0.9);
+		let alpha2 = consts::PI * 2. - alpha1;
+		Shape::new_triangle(radius, alpha1, alpha2)
+	}
+
+	fn eq_triangle(&mut self) -> Shape {
+		let radius = self.next_float(0.5, 1.0);
+		let alpha1 = consts::PI * 2. / 3.;
+		let alpha2 = consts::PI * 2. - alpha1;
+		Shape::new_triangle(radius, alpha1, alpha2)
+	}
+
+	fn star(&mut self) -> Shape {
+		let radius: f32 = self.next_float(1.0, 2.0);
+		let n = self.next_integer(3, 8);
+		let ratio1 = self.next_float(0.5, 1.0);
+		let ratio2 = self.next_float(0.7, 0.9) * (1. / ratio1);
+		Shape::new_star(n, radius, ratio1, ratio2)
+	}
+
+	fn poly(&mut self, upside_down: bool) -> Shape {
+		let n = self.next_integer(3, 8);
+		self.npoly(n, upside_down)
+	}
+
+	fn npoly(&mut self, n: AttachmentIndex, upside_down: bool) -> Shape {
+		let radius: f32 = self.next_float(1.0, 2.0);
+		let ratio1 = f32::cos(consts::PI / n as f32);
+		let ratio2 = 1. / ratio1;
+		if upside_down {
+			Shape::new_star(n, radius * ratio1, ratio2, ratio1)
+		} else {
+			Shape::new_star(n, radius, ratio1, ratio2)
+		}
+	}
+}
+
+pub struct Randomizer {
+	rng: rand::ThreadRng,
+}
+
+impl Gen for Randomizer {
+	fn new() -> Self {
+		Randomizer { rng: rand::thread_rng() }
+	}
+
+	fn next_float<T>(&mut self, min: T, max: T) -> T
+		where T: rand::Rand + num::Float {
+		self.rng.gen::<T>() * (max - min) + min
+	}
+
+	fn next_integer<T>(&mut self, min: T, max: T) -> T
+		where T: rand::Rand + num::Integer + Copy {
+		self.rng.gen::<T>() % (max - min + T::one()) + min
+	}
+}
+
+impl Generator for Randomizer {}
--- src/backend/world/mod.rs
+++ src/backend/world/mod.rs
@@ -1,114 +1,32 @@
 pub mod segment;
 pub mod agent;
+pub mod gen;
 
 use backend::obj;
 use backend::obj::*;
-use rand;
-use rand::Rng;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::f32::consts;
-use num;
 use core::color;
 use core::color::ToRgb;
 use core::geometry::*;
 use backend::world::segment::*;
 use backend::world::agent::Agent;
+use backend::world::gen::*;
 
 pub struct Swarm {
 	seq: Id,
-	rnd: Randomizer,
+	seq_tag: Id,
+	gen: Randomizer,
 	agents: HashMap<Id, agent::Agent>,
 }
 
-struct Randomizer {
-	rng: rand::ThreadRng,
-}
-
-#[allow(dead_code)]
-impl Randomizer {
-	fn new() -> Self {
-		Randomizer { rng: rand::thread_rng() }
-	}
-
-	fn frand<T>(&mut self, min: T, max: T) -> T
-		where T: rand::Rand + num::Float {
-		self.rng.gen::<T>() * (max - min) + min
-	}
-
-	fn irand<T>(&mut self, min: T, max: T) -> T
-		where T: rand::Rand + num::Integer + Copy {
-		self.rng.gen::<T>() % (max - min + T::one()) + min
-	}
-
-	fn random_ball(&mut self) -> Shape {
-		let radius: f32 = self.frand(1.0, 2.0);
-		Shape::new_ball(radius)
-	}
-
-	fn random_box(&mut self) -> Shape {
-		let radius: f32 = self.frand(1.0, 2.0);
-		let ratio: f32 = self.frand(1.0, 2.0);
-		Shape::new_box(radius, ratio)
-	}
-
-	fn random_vbar(&mut self) -> Shape {
-		let radius: f32 = self.frand(1.0, 2.0);
-		let ratio: f32 = self.frand(0.1, 0.2);
-		Shape::new_box(radius, ratio)
-	}
-
-	fn random_triangle(&mut self) -> Shape {
-		let radius = self.frand(0.5, 1.0);
-		let alpha1 = self.frand(consts::PI * 0.5, consts::PI * 0.9);
-		let alpha2 = consts::PI * 1.5 - self.frand(0., consts::PI);
-		Shape::new_triangle(radius, alpha1, alpha2)
-	}
-
-	fn random_iso_triangle(&mut self) -> Shape {
-		let radius = self.frand(0.5, 1.0);
-		let alpha1 = self.frand(consts::PI * 0.5, consts::PI * 0.9);
-		let alpha2 = consts::PI * 2. - alpha1;
-		Shape::new_triangle(radius, alpha1, alpha2)
-	}
-
-	fn random_eq_triangle(&mut self) -> Shape {
-		let radius = self.frand(0.5, 1.0);
-		let alpha1 = consts::PI * 2. / 3.;
-		let alpha2 = consts::PI * 2. - alpha1;
-		Shape::new_triangle(radius, alpha1, alpha2)
-	}
-
-	fn random_star(&mut self) -> Shape {
-		let radius: f32 = self.frand(1.0, 2.0);
-		let n = self.irand(3, 8);
-		let ratio1 = self.frand(0.5, 1.0);
-		let ratio2 = self.frand(0.7, 0.9) * (1. / ratio1);
-		Shape::new_star(n, radius, ratio1, ratio2)
-	}
-
-	fn random_poly(&mut self, upside_down: bool) -> Shape {
-		let n = self.irand(3, 8);
-		self.random_npoly(n, upside_down)
-	}
-
-	fn random_npoly(&mut self, n: AttachmentIndex, upside_down: bool) -> Shape {
-		let radius: f32 = self.frand(1.0, 2.0);
-		let ratio1 = f32::cos(consts::PI / n as f32);
-		let ratio2 = 1. / ratio1;
-		if upside_down {
-			Shape::new_star(n, radius * ratio1, ratio2, ratio1)
-		} else {
-			Shape::new_star(n, radius, ratio1, ratio2)
-		}
-	}
-}
-
 impl Swarm {
-	pub fn new() -> Swarm {
+	pub fn new(seq_tag: Id) -> Swarm {
 		Swarm {
 			seq: 0,
-			rnd: Randomizer::new(),
+			seq_tag: seq_tag,
+			gen: Randomizer::new(),
 			agents: HashMap::new(),
 		}
 	}
@@ -123,12 +41,14 @@ impl Swarm {
 
 	pub fn next_id(&mut self) -> Id {
 		self.seq = self.seq + 1;
-		self.seq
+		self.seq << 8 + self.seq_tag
 	}
 
 	pub fn new_resource(&mut self, initial_pos: Position, charge: f32) -> Id {
-		let albedo = color::YPbPr::new(0.5, self.rnd.frand(-0.5, 0.5), self.rnd.frand(-0.5, 0.5));
-		let ball = self.rnd.random_ball();
+		let albedo = color::YPbPr::new(0.5,
+		                               self.gen.next_float(-0.5, 0.5),
+		                               self.gen.next_float(-0.5, 0.5));
+		let ball = self.gen.ball();
 		let mut builder = agent::AgentBuilder::new(self.next_id(),
 		                                           Material { density: 1.0, ..Default::default() },
 		                                           Livery { albedo: albedo.to_rgba(), ..Default::default() },
@@ -153,16 +73,16 @@ impl Swarm {
 	}
 
 	pub fn new_minion(&mut self, initial_pos: Position, charge: f32) -> Id {
-		let albedo = color::Hsl::new(self.rnd.frand(0., 1.), 0.5, 0.5);
+		let albedo = color::Hsl::new(self.gen.next_float(0., 1.), 0.5, 0.5);
 		let mut builder = agent::AgentBuilder::new(self.next_id(),
 		                                           Material { density: 0.2, ..Default::default() },
 		                                           Livery { albedo: albedo.to_rgba(), ..Default::default() },
 		                                           segment::State::with_charge(0., charge, charge));
-		let arm_shape = self.rnd.random_star();
-		let leg_shape = self.rnd.random_star();
-		let torso_shape = self.rnd.random_npoly(5, true);
-		let head_shape = self.rnd.random_iso_triangle();
-		let tail_shape = self.rnd.random_vbar();
+		let arm_shape = self.gen.star();
+		let leg_shape = self.gen.star();
+		let torso_shape = self.gen.npoly(5, true);
+		let head_shape = self.gen.iso_triangle();
+		let tail_shape = self.gen.vbar();
 		let initial_angle = consts::PI / 2. + f32::atan2(initial_pos.y, initial_pos.x);
 
 		let torso = builder.start(initial_pos, initial_angle, &torso_shape)
@@ -176,12 +96,12 @@ impl Swarm {
 
 		let mut belly = torso;
 		let mut belly_mid = torso_shape.mid();
-		for _ in 0..self.rnd.irand(0, 4) {
-			let belly_shape = self.rnd.random_poly(true);
+		for _ in 0..self.gen.next_integer(0, 4) {
+			let belly_shape = self.gen.poly(true);
 
 			belly = builder.add(belly, belly_mid, &belly_shape, BELLY | JOINT).index();
 			belly_mid = belly_shape.mid();
-			if self.rnd.irand(0, 4) == 0 {
+			if self.gen.next_integer(0, 4) == 0 {
 				builder.addr(belly, 2, &arm_shape, ARM | ACTUATOR | RUDDER)
 					.addl(belly, -2, &arm_shape, ARM | ACTUATOR | RUDDER);
 			}
@@ -293,13 +213,13 @@ impl World {
 		World {
 			extent: Rect::new(-50., -50., 50., 50.),
 			fence: Mesh::from_shape(Shape::new_ball(50.), Winding::CW),
-			players: Swarm::new(),
-			minions: Swarm::new(),
-			friendly_fire: Swarm::new(),
-			enemies: Swarm::new(),
-			enemy_fire: Swarm::new(),
-			resources: Swarm::new(),
-			props: Swarm::new(),
+			minions: Swarm::new(0),
+			players: Swarm::new(1),
+			friendly_fire: Swarm::new(2),
+			enemies: Swarm::new(3),
+			enemy_fire: Swarm::new(4),
+			resources: Swarm::new(5),
+			props: Swarm::new(6),
 		}
 	}
 
