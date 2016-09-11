8870128e519a3beaad7475cd39163ade3bedbdcb
Nicola Orru <nigu.orru@gmail.com>
2016-09-11 15:00:19 +0100

    Added simple proprioception to neural network

--- src/backend/systems/ai.rs
+++ src/backend/systems/ai.rs
@@ -59,13 +59,14 @@ impl AiSystem {
 
 		for (_, agent) in minions.iter_mut() {
 			let brain = agent.brain().clone();
+			let torso = agent.first_segment(segment::STORAGE);
 			let head = agent.first_segment(segment::SENSOR);
 			if let Some(sensor) = head {
 				let p0 = sensor.transform.position;
 				let radar_range = sensor.mesh.shape.radius() * 10.;
 				let current_target = agent.state.target().clone();
 				let current_target_position = agent.state.target_position().clone();
-
+				// if our original target is dead then we need to find another one
 				let new_target: Option<(obj::Id, Position)> = match current_target {
 					None => {
 						targets.iter()
@@ -74,50 +75,57 @@ impl AiSystem {
 					}
 					Some(id) => targets.get(&id).map(|&position| (id, position)),
 				};
-
+				// and failing that again, we target
 				match new_target {
 					None => agent.state.retarget(None, *nearest_beacon(beacons, &current_target_position)),
 					Some((id, position)) => agent.state.retarget(Some(id), position),
 				};
-				// where oid's target is in the world
+				// find where our target is in the world
 				let target_position = agent.state.target_position().clone();
-				// where oid's target is compared to the oid's head
+				// and transform the world position into the head's frame
 				let t0 = target_position - sensor.transform.position;
 				let t = t0.normalize_to(t0.length().min(radar_range));
 				// direction in which the head is pointing, normalized
 				let s = Matrix2::from_angle(rad(sensor.transform.angle)) * (-Position::unit_y());
+				// some proprioception, feeding back the angle betweent the neck and the first torso
+				let neck_angle = sensor.transform.angle -
+				                 torso.map(|t| t.transform.angle).unwrap_or(sensor.transform.angle);
 				// we pass the relative position of the target decomposed in our frame of reference to the neural network
 				// expecting four components we can use as thresholds
-				let r = agent.brain().response(&[0., t.dot(s), t.perp_dot(s), 0.]);
+				let r = agent.brain().response(&[neck_angle, t.dot(s), t.perp_dot(s), 0.]);
 				const POWER_BOOST: f32 = 100.;
 
 				let segments = &mut agent.segments_mut();
 				for segment in segments.iter_mut() {
-					if segment.flags.intersects(segment::ACTUATOR) {
+					let flags = &segment.flags;
+					if flags.contains(segment::ACTUATOR) {
+						info!("Actuator flags set {:?}", flags);
 						let power = segment.state.get_charge() * segment.mesh.shape.radius().powi(2) * POWER_BOOST;
 						let f = Matrix2::from_angle(rad(segment.transform.angle)) * Position::unit_y() * power;
-						let intent = if let Some(refs) = segment.state.last_touched {
-							let fear: f32 = brain.fear();
-							match refs.id().type_of() {
-								agent::AgentType::Resource => Intent::Idle,
-								_ => Intent::RunAway(f * fear),
-							}
-						} else if segment.flags.contains(segment::RUDDER | segment::LEFT) &&
-						                       r[0] > brain.hunger() {
-							Intent::Move(f)
-						} else if segment.flags.contains(segment::RUDDER | segment::RIGHT) &&
-						                       r[1] > brain.hunger() {
-							Intent::Move(f)
-						} else if segment.flags.contains(segment::THRUSTER) && r[2] > brain.haste() {
-							Intent::Move(f)
-						} else if segment.flags.contains(segment::BRAKE) && r[3] > brain.prudence() {
-							Intent::Move(-f)
-						} else {
-							Intent::Idle
-						};
+						let intent =
+							if let Some(refs) = segment.state.last_touched {
+								match refs.id().type_of() {
+									agent::AgentType::Resource => Intent::Idle,
+									_ => {
+										let fear: f32 = brain.fear();
+										Intent::RunAway(f * fear)
+									}
+								}
+							} else if flags.contains(segment::RUDDER | segment::LEFT) && r[0] > brain.hunger() {
+								Intent::Move(f)
+							} else if flags.contains(segment::RUDDER | segment::RIGHT) && r[1] > brain.hunger() {
+								Intent::Move(f)
+							} else if flags.contains(segment::THRUSTER) && r[2] > brain.haste() {
+								Intent::Move(f)
+							} else if flags.contains(segment::BRAKE) && r[3] > brain.prudence() {
+								Intent::Brake(f)
+							} else {
+								Intent::Idle
+							};
 						match intent {
 							Intent::Idle => segment.state.set_target_charge(brain.rest()),
 							Intent::Move(_) => segment.state.set_target_charge(brain.thrust()),
+							Intent::Brake(_) => segment.state.set_target_charge(brain.thrust()),
 							Intent::RunAway(_) => segment.state.set_charge(brain.thrust()),
 						}
 						segment.state.intent = intent;
