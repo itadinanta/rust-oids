57675918cd28a4ae1d4ae47da073470223aa878b
Nicola Orru <nigu.orru@gmail.com>
2017-12-28 22:42:04 +0000

    Reformat

--- src/frontend/render/formats.rs
+++ src/frontend/render/formats.rs
@@ -8,20 +8,22 @@ pub type RenderColorChannels = gfx::format::R16_G16_B16_A16;
 pub type RenderColorFormat = (RenderColorChannels, gfx::format::Float);
 pub type RenderDepthFormat = gfx::format::Depth;
 
-pub type ScreenColorChannels = gfx::format::R8_G8_B8_A8; // Srgba8 broken on Linux
-pub type ScreenColorFormat = (ScreenColorChannels, gfx::format::Unorm); // Srgba8 broken on Linux
+pub type ScreenColorChannels = gfx::format::R8_G8_B8_A8;
+// Srgba8 broken on Linux
+pub type ScreenColorFormat = (ScreenColorChannels, gfx::format::Unorm);
+// Srgba8 broken on Linux
 pub type ScreenDepthFormat = gfx::format::Depth;
 
 pub type RenderSurface<R> = (gfx::handle::Texture<R, RenderColorChannels>,
-                             gfx::handle::ShaderResourceView<R, Float4>,
-                             gfx::handle::RenderTargetView<R, RenderColorFormat>);
+							 gfx::handle::ShaderResourceView<R, Float4>,
+							 gfx::handle::RenderTargetView<R, RenderColorFormat>);
 
 pub type DepthSurface<R> = (gfx::handle::Texture<R, gfx::format::D24>,
-                            gfx::handle::ShaderResourceView<R, Float>,
-                            gfx::handle::DepthStencilView<R, RenderDepthFormat>);
+							gfx::handle::ShaderResourceView<R, Float>,
+							gfx::handle::DepthStencilView<R, RenderDepthFormat>);
 
 pub type RenderSurfaceWithDepth<R> = (gfx::handle::ShaderResourceView<R, Float4>,
-                                      gfx::handle::RenderTargetView<R, RenderColorFormat>,
-                                      gfx::handle::DepthStencilView<R, RenderDepthFormat>);
+									  gfx::handle::RenderTargetView<R, RenderColorFormat>,
+									  gfx::handle::DepthStencilView<R, RenderDepthFormat>);
 
 pub const MSAA_MODE: gfx::texture::AaMode = gfx::texture::AaMode::Multi(4);
