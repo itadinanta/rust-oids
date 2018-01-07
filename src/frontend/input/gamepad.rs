72bb252dbb94633e811639cda2d65f69eaf6409d
Nicola Orru <nigu.orru@gmail.com>
2018-01-07 03:00:12 +0000

    Removed repeat filtering for gamepad button pressed

--- src/frontend/input/gamepad.rs
+++ src/frontend/input/gamepad.rs
@@ -1,12 +1,12 @@
 use gilrs;
 use gilrs::Gilrs;
-use gilrs::ev::filter::{Filter, Repeat};
+// use gilrs::ev::filter::{Filter, Repeat};
 
 use frontend::input;
 use frontend::input::EventMapper;
 
 pub struct GamepadEventLoop {
-	repeat_filter: Repeat,
+	//	repeat_filter: Repeat,
 	gilrs: Gilrs,
 }
 
@@ -18,16 +18,21 @@ impl input::EventMapper<gilrs::Event> for GamepadEventLoop {
 
 impl GamepadEventLoop {
 	pub fn new() -> Self {
+		let gilrs = Gilrs::new();
+		for (_id, gamepad) in gilrs.gamepads() {
+			println!("{} is {:?}", gamepad.name(), gamepad.power_info());
+		}
 		GamepadEventLoop {
-			repeat_filter: Repeat::new(),
-			gilrs: Gilrs::new(),
+//			repeat_filter: Repeat::new(),
+			gilrs,
 		}
 	}
 
 	pub fn poll_events<F>(&mut self, mut on_input_event: F)
 		where F: FnMut(input::Event) {
-		let repeat_filter = self.repeat_filter.clone();
-		while let Some(ev) = Filter::filter(&self.gilrs.next_event(), &repeat_filter, &self.gilrs) {
+		//let repeat_filter = self.repeat_filter.clone();
+		//while let Some(ev) = Filter::filter(&self.gilrs.next_event(), &repeat_filter, &self.gilrs) {
+		while let Some(ev) = self.gilrs.next_event() {
 			self.gilrs.update(&ev);
 			println!("{:?}", ev);
 			self.translate(&ev).map(&mut on_input_event);
