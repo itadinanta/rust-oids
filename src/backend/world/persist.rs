5e8ba39a818143f84e239e966779e2b1a2c6c56b
Nico Orru <nigu.orru@gmail.com>
2018-04-12 21:09:14 +0100

    To and from string

--- src/backend/world/persist.rs
+++ src/backend/world/persist.rs
@@ -1,3 +1,6 @@
+use backend::world;
+use serde_json;
+
 #[derive(Serialize, Deserialize, Debug)]
 struct Vector {
 	x: f32,
@@ -13,8 +16,8 @@ struct Transform {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Segment {
 	transform: Transform,
-	rest_angle: f32,
 	motion: Transform,
+	rest_angle: f32,
 	index: usize,
 	age_seconds: f64,
 	age_frames: usize,
@@ -49,4 +52,38 @@ pub struct World {
 	regenerations: usize,
 	minion_gene_pool: Vec<String>,
 	resource_gene_pool: Vec<String>,
+}
+
+pub struct Serializer;
+
+impl Serializer {
+	pub fn to_string(world: &world::World) -> String {
+		let swarms = Vec::new();
+
+		let s_world = World {
+			extent_min: Vector {
+				x: 0.0,
+				y: 0.0,
+			},
+			extent_max: Vector {
+				x: 0.0,
+				y: 0.0,
+			},
+			swarms,
+			regenerations: 0,
+			minion_gene_pool: Vec::new(),
+			resource_gene_pool: Vec::new(),
+
+		};
+		let serialized = serde_json::to_string(&s_world).unwrap();
+		serialized
+	}
+
+	pub fn from_string(source: &str, mut world: &world::World) {
+		let result: Result<World, _> = serde_json::from_str(source);
+		match result {
+			Ok(world) => {}
+			Err(_) => {}
+		}
+	}
 }
