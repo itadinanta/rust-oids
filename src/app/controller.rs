4b5595598d232ce7c479bf4372696f65268e2708
Nico Orru <nigu.orru@gmail.com>
2018-08-26 11:41:33 +0100

    Clean up of controller code

--- src/app/controller.rs
+++ src/app/controller.rs
@@ -9,6 +9,7 @@ use core::view::WorldTransform;
 use frontend::input;
 
 use super::events::Event::*;
+use frontend::input::Axis::*;
 use frontend::input::Key::*;
 
 const KEY_HELD_MAP: &[(input::Key, Event)] = &[(W, CamUp(1.)), (S, CamDown(1.)), (A, CamLeft(1.)), (D, CamRight(1.))];
@@ -60,17 +61,14 @@ pub trait InputController {
 }
 
 impl DefaultController {
-	fn events_on_key_held<I>(input_state: &I, events: &mut Vec<Event>)
+	fn interpret_key_press<I>(input_state: &I, events: &mut Vec<Event>)
 	where I: input::InputRead {
 		for (key_held, event) in KEY_HELD_MAP {
 			if input_state.key_pressed(*key_held) {
 				events.push(*event);
 			}
 		}
-	}
 
-	fn events_on_key_pressed_once<I>(input_state: &I, events: &mut Vec<Event>)
-	where I: input::InputRead {
 		for (key_pressed, event) in KEY_PRESSED_ONCE_MAP {
 			if input_state.key_pressed(*key_pressed) {
 				events.push(*event);
@@ -78,7 +76,7 @@ impl DefaultController {
 		}
 	}
 
-	fn events_on_mouse_move<V, W, I>(
+	fn interpret_mouse_move<V, W, I>(
 		input_state: &I,
 		events: &mut Vec<Event>,
 		view_transform: &V,
@@ -94,11 +92,11 @@ impl DefaultController {
 		let mouse_view_pos = view_transform.to_view(mouse_window_pos);
 		let mouse_world_pos = world_transform.to_world(mouse_view_pos);
 
-		if input_state.key_once(input::Key::MouseLeft) && input_state.any_ctrl_pressed() {
+		if input_state.key_once(MouseLeft) && input_state.any_ctrl_pressed() {
 			events.push(Event::PickMinion(mouse_world_pos));
 		};
 
-		if input_state.key_once(input::Key::MouseMiddle) {
+		if input_state.key_once(MouseMiddle) {
 			if input_state.any_ctrl_pressed() {
 				events.push(Event::RandomizeMinion(mouse_world_pos));
 			} else {
@@ -130,57 +128,61 @@ impl DefaultController {
 		mouse_world_pos
 	}
 
-	fn events_on_gamepad<I>(input_state: &I, events: &mut Vec<Event>, mouse_world_pos: Position)
+	fn interpret_trigger_fire<I>(input_state: &I, events: &mut Vec<Event>)
 	where I: input::InputRead {
-		let mouse_left_pressed = input_state.key_pressed(input::Key::MouseLeft) && !input_state.any_ctrl_pressed();
-
-		let firerate = input_state.gamepad_axis(0, input::Axis::L2);
-		let firepower = input_state.gamepad_axis(0, input::Axis::R2);
+		let mouse_left_pressed = input_state.key_pressed(MouseLeft) && !input_state.any_ctrl_pressed();
+		let firerate = input_state.gamepad_axis(0, L2);
+		let firepower = input_state.gamepad_axis(0, R2);
 		if firepower >= DEAD_ZONE {
 			events.push(Event::PrimaryTrigger(firepower, f64::from(firerate)));
-		} else if input_state.key_pressed(input::Key::Space) || mouse_left_pressed {
+		} else if input_state.key_pressed(Space) || mouse_left_pressed {
 			events.push(Event::PrimaryTrigger(1.0, 1.0));
 		}
-		let thrust = Position {
-			x: if input_state.key_pressed(input::Key::Right) {
-				1.
-			} else if input_state.key_pressed(input::Key::Left) {
-				-1.
-			} else {
-				input_state.gamepad_axis(0, input::Axis::LStickX)
-			},
+	}
 
-			y: if input_state.key_pressed(input::Key::Up) {
-				1.
-			} else if input_state.key_pressed(input::Key::Down) {
-				-1.
-			} else {
-				input_state.gamepad_axis(0, input::Axis::LStickY)
-			},
+	fn interpret_movement<I>(input_state: &I, events: &mut Vec<Event>, mouse_world_pos: Position)
+	where I: input::InputRead {
+		let yaw = Position {
+			x: input_state.gamepad_axis(0, RStickX),
+			y: input_state.gamepad_axis(0, RStickY),
 		};
 
-		let yaw = Position {
-			x: input_state.gamepad_axis(0, input::Axis::RStickX),
-			y: input_state.gamepad_axis(0, input::Axis::RStickY),
+		let thrust = Position {
+			x: if input_state.key_pressed(Right) {
+				1.
+			} else if input_state.key_pressed(Left) {
+				-1.
+			} else {
+				input_state.gamepad_axis(0, LStickX)
+			},
+
+			y: if input_state.key_pressed(Up) {
+				1.
+			} else if input_state.key_pressed(Down) {
+				-1.
+			} else {
+				input_state.gamepad_axis(0, LStickY)
+			},
 		};
 
 		use cgmath::InnerSpace;
 		let magnitude = thrust.magnitude2();
+		let mouse_left_pressed = input_state.key_pressed(MouseLeft) && !input_state.any_ctrl_pressed();
 		events.push(Event::VectorThrust(
 			if magnitude >= DEAD_ZONE {
 				Some(thrust / magnitude.max(1.))
 			} else {
 				None
 			},
-			if input_state.key_pressed(input::Key::PageUp) {
+			if input_state.key_pressed(PageUp) {
 				VectorDirection::Turn(TURN_SPEED)
-			} else if input_state.key_pressed(input::Key::PageDown) {
+			} else if input_state.key_pressed(PageDown) {
 				VectorDirection::Turn(-TURN_SPEED)
 			} else if yaw.magnitude() >= DEAD_ZONE {
 				VectorDirection::Orientation(yaw)
 			} else if mouse_left_pressed {
 				VectorDirection::LookAt(mouse_world_pos)
-			} else if thrust.magnitude2() > 0.1 {
+			} else if magnitude > 0.1 {
 				VectorDirection::FromVelocity
 			} else {
 				VectorDirection::None
@@ -197,11 +199,10 @@ impl InputController for DefaultController {
 		I: input::InputRead, {
 		let mut events = Vec::new();
 
-		DefaultController::events_on_key_held(input_state, &mut events);
-		DefaultController::events_on_key_pressed_once(input_state, &mut events);
-		let mouse_world_pos =
-			DefaultController::events_on_mouse_move(input_state, &mut events, view_transform, world_transform, dt);
-		DefaultController::events_on_gamepad(input_state, &mut events, mouse_world_pos);
+		Self::interpret_key_press(input_state, &mut events);
+		let mouse_world_pos = Self::interpret_mouse_move(input_state, &mut events, view_transform, world_transform, dt);
+		Self::interpret_trigger_fire(input_state, &mut events);
+		Self::interpret_movement(input_state, &mut events, mouse_world_pos);
 
 		events
 	}
